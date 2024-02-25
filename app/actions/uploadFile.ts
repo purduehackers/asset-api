f504703206c833f2ac65a9248f040b3a91ba730f
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-02-25 15:45:32 -0500

    basic uploading and downloading image

---
+++ app/actions/uploadFile.ts
@@ -0,0 +1,40 @@
+"use server"
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const S3 = new S3Client({
+    region: "auto",
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY as string,
+        secretAccessKey: process.env.R2_ACCESS_SECRET as string,
+    },
+});  
+
+export const uploadFile = async (formData: FormData) => {
+    const preSignedUrl = await getSignedUrl(
+        S3,
+        new PutObjectCommand(
+            { 
+                Bucket: process.env.R2_UPLOAD_BUCKET,
+                Key: formData.get('filename') as string
+            }), {
+                expiresIn: 3600
+            })
+    return preSignedUrl
+}
+
+
+export const getFile = async (filename: string) => {
+    const preSignedUrl = await getSignedUrl(
+        S3,
+        new GetObjectCommand(
+            { 
+                Bucket: process.env.R2_UPLOAD_BUCKET,
+                Key: filename
+            }), {
+                expiresIn: 3600
+            })
+    return preSignedUrl
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "asset.3f7a7e1b4e84b05d7069019ba14e187f.r2.cloudflarestorage.com"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
