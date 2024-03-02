2fc2c8377086feefd4b086ab63f5a4a65ce5793d
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-03-02 17:54:46 -0500

    add tailwind

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 import Copy from '../components/copy'
-import styles from './styles.module.css'
 
 const ProjectPage = () => {
   const [keys, setKeys] = useState([])
@@ -21,34 +20,57 @@ const ProjectPage = () => {
   }, [])
 
   return (
-    <table className={styles.AssetsTable}>
-      <thead>
-        <tr>
-          <th>slug</th>
-          <th>preview</th>
-          <th>url</th>
-          <th>action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {keys &&
-          keys.map((key) => {
-            const url = `http://localhost:3000/api/${key}`
-            return (
-              <tr key={key}>
-                <th>{key}</th>
-                <th>
-                  <Image src={url} alt={key} width={100} height={100} />
-                </th>
-                <th>
-                  {url} <Copy url={url} />
-                </th>
-                <th>edit</th>
-              </tr>
-            )
-          })}
-      </tbody>
-    </table>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="relative w-1/2 overflow-x-auto shadow-md rounded-xl">
+        <table className="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                key
+              </th>
+              <th scope="col" className="px-6 py-3">
+                preview
+              </th>
+              <th scope="col" className="px-6 py-3">
+                url
+              </th>
+              <th scope="col" className="px-6 py-3 text-center">
+                action
+              </th>
+            </tr>
+          </thead>
+          {keys &&
+            keys.map((key) => {
+              const url = `${process.env.NEXT_PUBLIC_APP_URL}/api/${key}`
+              return (
+                <tr className="bg-gray-900 border-b border-gray-700" key={key}>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {key}
+                  </th>
+                  <td className="px-6 py-4">
+                    <Image src={url} alt={key} width={100} height={100} />
+                  </td>
+                  <td className="px-6 py-4">
+                    {url} <Copy url={url} />
+                  </td>
+                  <td className="px-6 py-4 text-center">
+                    <a
+                      href="#"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              )
+            })}
+        </table>
+      </div>
+      <table></table>
+    </div>
   )
 }
 
